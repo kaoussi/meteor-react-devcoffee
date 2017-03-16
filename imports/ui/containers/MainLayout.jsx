52525967b33da8547a3def6def85b669336467bc
kaoussi <kaoussi.souhail@gmail.com>
2017-03-16 00:23:10 +0000

    added materialize

--- imports/ui/containers/MainLayout.jsx
+++ imports/ui/containers/MainLayout.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Header from '../components/Header.jsx';
 import Home from '../pages/Home.jsx';
-import About from '../pages/About.jsx';
 import NotFound from '../pages/NotFound.jsx';
 
 export default class MainLayout extends React.Component {
@@ -14,7 +13,6 @@ export default class MainLayout extends React.Component {
           <Header />
             <Switch>
               <Route exact path='/' component={Home} />
-              <Route path = '/about' component={About} />
               <Route component={NotFound} />
             </Switch>
         </div>
